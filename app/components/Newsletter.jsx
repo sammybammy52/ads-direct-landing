52907c1166918f63f483aba7d4141d841272bcff
Aselemi Divine <aselemidivine@gmail.com>
2023-07-24 12:08:46 +0100

    added gradient to the newsletter section

--- app/components/Newsletter.jsx
+++ app/components/Newsletter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <section className="bg-primary py-6">
+  <section className="bg-grad-primary py-6">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-md sm:text-center">
       <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-white">Sign up for our newsletter</h2>
